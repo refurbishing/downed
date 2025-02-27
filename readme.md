4a5e312e805d431f5f88b618254c70673ff17d56
Cortex <126973723+verysillycat@users.noreply.github.com>
2025-02-26 19:12:13 -0600

    init

---
+++ readme.md
@@ -0,0 +1,9 @@
+# downed
+
+## what is this?
+
+this is a simple website that displays a countdown to the next time vercel will allow me to create a new account.
+
+## why?
+
+i tired switching to cloudflare pages for [my website](https://github.com/refurbishing/website) and it was pain due to sizes limits, but before that i deleted my vercel account which is a big mistake and couldn't revert back which resulted in me waiting for vercel account cooldown.. so thought making this because it's way nicer then a cloudflare 522 error code page.
---
+++ script.js
@@ -0,0 +1,31 @@
+function updateCountdown() {
+//    wink wink
+//    const targetDate = new Date();
+//    targetDate.setDate(targetDate.getDate() + 8);
+//    targetDate.setHours(0, 0, 0, 0);
+//    targetDate.setHours(targetDate.getHours() - 6);
+
+    const targetDate = new Date('March 6, 2025 00:00:00');
+
+    function formatTimeRemaining(distance) {
+        const days = Math.ceil(distance / (1000 * 60 * 60 * 24));
+        if (days === 0) return 'today';
+        return `${days} ${days === 1 ? 'day' : 'days'}`;
+    }
+
+    function tick() {
+        const now = new Date();
+        const distance = targetDate - now;
+
+        if (distance < 0) {
+            document.getElementById('countdown').textContent = "today";
+            return;
+        }
+
+        document.getElementById('countdown').textContent = formatTimeRemaining(distance);
+    }
+
+    tick();
+    setInterval(tick, 1000);
+}
+document.addEventListener('DOMContentLoaded', updateCountdown);
